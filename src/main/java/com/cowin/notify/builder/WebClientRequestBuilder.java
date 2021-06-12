4cd3f83cd8f0cf6b8a3fe5e429040f21c8be4310
Vaisakh <ysakhpr@gmail.com>
2021-06-12 13:15:30 +0530

    httpclien

---
+++ src/main/java/com/cowin/notify/builder/WebClientRequestBuilder.java
@@ -0,0 +1,49 @@
+package com.cowin.notify.builder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.cowin.notify.model.Center;
+import com.cowin.notify.model.CenterList;
+
+
+
+@Service
+public class WebClientRequestBuilder {
+
+	Logger log = LoggerFactory.getLogger(WebClientRequestBuilder.class);
+
+	private static final String COWIN_API_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?";
+
+	public HttpResponse getCowinDetails(Integer pincode,String date) throws IOException, InterruptedException {
+		
+		
+		
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.header("accept","application/json" )
+				.GET()
+				.uri(URI.create(COWIN_API_URL+"pincode="+pincode+"&date="+date))
+				.build();
+		
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		
+		log.info(COWIN_API_URL+"pincode="+pincode+"&date"+date+response.body().toString());
+				
+		return response;
+	}
+
+}
