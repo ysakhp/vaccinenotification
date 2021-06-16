12a6e37d212815c3fcc358232855b4145c295de1
Vaisakh <ysakhpr@gmail.com>
2021-06-16 23:48:52 +0530

    Memory Status Service

---
+++ src/main/java/com/cowin/notify/service/MemoryService.java
@@ -0,0 +1,20 @@
+package com.cowin.notify.service;
+
+import org.springframework.stereotype.Service;
+
+import com.cowin.notify.model.MemoryStats;
+
+@Service
+public class MemoryService {
+
+	public MemoryStats getMemoryStatus() {
+		MemoryStats memStats = new MemoryStats();
+		memStats.setHeapFreeSize(Runtime.getRuntime().freeMemory());
+		memStats.setHeapMaxSize(Runtime.getRuntime().maxMemory());
+		memStats.setHeapSize(Runtime.getRuntime().totalMemory());
+		return memStats;
+	}
+	
+	
+
+}
